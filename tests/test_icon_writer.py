315a7ce62d4fe166b08baf6604ea92d5e563ea69
Pamela Fox <pamela.fox@gmail.com>
2022-09-21 07:09:21 -0700

    black

--- tests/test_icon_writer.py
+++ tests/test_icon_writer.py
@@ -2,11 +2,13 @@ from PIL import Image, ImageFont
 
 from icon_writer import get_text_size, find_font_size, write_icon
 
+
 def test_get_text_size():
     text = "AZURE"
     image = Image.new("RGB", (80, 80))
     font = ImageFont.truetype("assets/OpenSans-Bold.ttf", 100)
     assert get_text_size(text, image, font) == (325, 108)
+
 
 def test_find_font_size():
     text = "AZURE"
@@ -14,6 +16,7 @@ def test_find_font_size():
     image = Image.new("RGB", (80, 80))
     width_ratio = 0.7
     assert find_font_size(text, font_family, image, width_ratio) == 17
+
 
 def test_write_icon():
     image = write_icon("hi")
