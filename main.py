0b4fc56b6150847b9a6418510e600fe54ba523c6
Pamela Fox <pamela.fox@gmail.com>
2022-09-21 07:44:26 -0700

    Move into init

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.icon_writer_package.icon_writer import write_icon
+from src.icon_writer_package import write_icon
 
 # write_icon("TEAMS", bgcolor=(95, 91, 204), fontcolor='white')
 # write_icon("EDGE", bgcolor=(45, 248, 141), fontcolor=(2, 66, 162))
